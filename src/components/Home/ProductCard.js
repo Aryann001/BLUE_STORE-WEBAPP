d5e74a44b173f0072ab5460ce963c030507b5f14
Aryann001 <140709829+Aryann001@users.noreply.github.com>
2023-10-22 18:32:05 +0530

    Update ProductCard.js

--- src/components/Home/ProductCard.js
+++ src/components/Home/ProductCard.js
@@ -18,7 +18,7 @@ const ProductCard = ({ product }) => {
       <div>
         <Rating {...options} /> <span className="productCardSpan">({product.numOfReviews} Reviews)</span>
       </div>
-      {product.featured && (
+      {product.featured === true && (
         <div className="featuredBadge">
           <div><PiMedalFill /></div>
           <p>Featured</p>
